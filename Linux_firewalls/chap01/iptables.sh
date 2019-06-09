497f6d69356fbfa86fc764af1f11b7f6289358e8
tux <tux@localhost.localdomain>
2019-06-09 22:35:33 +0800

    learning at 20190609 22:00

--- Linux_firewalls/chap01/iptables.sh
+++ Linux_firewalls/chap01/iptables.sh
@@ -23,9 +23,52 @@ $MODPROBE ip_nat_ftp
 ###### INPUT chain ######
 echo "[+] Setting up INPUT chain ..."
 ### state tracking rules
+### Packets that do not match a valid state should be logged and dropped early. The state match is used by each of the rules, along with the criteria of INVALID, ESTABLISHED, or RELATED. 
+### The INVALID state applies to packets thatcannot be identified as belonging to any existing connection-- for example, a TCP FIN packet that arrives out of the blue(i.e., when it is not a part of any TCP session) would match the INVALID state. 
+### The ESTABLISHED state triggers on packets only after the Netfilter connection-tracking subsystem has seen packets in both directions(such as acknowledgement packets in a TCP connection through which data is being exchanged).
+### The RELATED state describes packets that are starting a new connection (Here connection is the tracking mechanism that Netfilter uses to categorize packets) in the Netfilter connection-tracking subsystem, but this connection is associated with an existing one--for example, an ICMP Port Unreachable message that is returned after a pachet is sent to a UDP socket where no server is bound.
 $IPTABLES -A INPUT -m state --state INVALID -j LOG --log-prefix "DROP INVALID " --log-ip-options --log-tcp-options
 $IPTABLES -A INPUT -m state --state INVALID -j DROP
 $IPTABLES -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
+### anti-spoofing rules
+### anti-spoofing rules are added here so packets that originate from the internal network must have a source address within the 192.168.10.0/24 subnet.
+$IPTABLES -A INPUT -i eth1 -s ! $INT_NET -j LOG --log-prefix "SPOOFED PKT "
+$IPTABLES -A INPUT -i eth1 -s ! $INT_NET -j DROP
+
+### ACCEPT rules
+### Two ACCEPT rules for SSH connections from the internal network, and ICMP Echo Requests are accepted from any source.
+### The rule that accepts SSH connections uses the state match with a state of NEW together with the iptables --syn command-line argument. This only matches on TCP packets with FIN, RST, and ACK flags zeroed-out and the SYN flag set, and then only if the NEW state is matched(which means that packet is starting a new connection, as far as the connection-tracking subsystem is concerned).
+
+$IPTABLES -A INPUT -i eth1 -p tcp -s $INT_NET --dport 22 --syn -m state --state NEW -j ACCEPT
+$IPTABLES -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
+
+### default INPUT LOG rule
+### Here is the default LOG rule. As you can see, the configuration of the INPUT chain is exceedingly easy, since we only need to accept incoming connnection requests to the SSH daemon from the internal network, enable state tracking for locally generated network traffic, and finally log and drop unwanted packets(including spoofed packets from the internal network)
+### One thing to note about the iptables.sh script is that all of the LOG rules are built with the --log-ip-options and --log-tcp-options command-line arguments. This allows the resulting iptables syslog messages to include the IP and TCP options portions of the IP and TCP headers if the packet that match the LOG rule contains them. This functionality is important for both attack detection and passive OS fingerprinting operations performed by psad(chapter 7)
+$IPTABLES -A INPUT -i ! lo -j LOG --log-prefix "DROP " --log-ip-options --log-tcp-options
 
 
+
+###### OUTPUT chain ######
+echo "[+] Setting up OUTPUT chain..."
+### state tracking rules
+$IPTABLES -A OUTPUT -m state INVALID -j LOG --log-prefix "DROP INVALID " --log-ip-options --log-tcp-options
+$IPTABLES -A OUTPUT -m state --state INVALID -j DROP
+$IPTABLES -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+### ACCEPT rules for allowing connections out
+### Assume that connections initiated from the firewall itself will be to download patches or software over FTP, HTTP, or HTTPS; to initiate outbound SSH and SMTP connections; or to issue DNS or whois queries against other systems.
+$IPTABLES -A OUTPUT -p tcp --dport 21 --syn -m state --state NEW -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp --dport 22 --syn -m state --state NEW -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp --dport 25 --syn -m state --state NEW -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp --dport 43 --syn -m state --state NEW -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp --dport 80 --syn -m state --state NEW -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp --dport 443 --syn -m state --state NEW -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp --dport 4321 --syn -m state --state NEW -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp --dport 53 --syn -m state --state NEW -j ACCEPT
+$IPTABLES -A OUTPUT -p udp --dport 53 --syn -m state --state NEW -j ACCEPT
+$IPTABLES -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
+
+### default OUTPUT LOG rule
+$IPTABLES -A OUTPUT -o ! lo -j LOG --log-prefix "DROP " --log-ip-optins --log-tcp-options
